143e75c9be98209d6f3eccb989a5e0f5eb2d0eec
Maurizio Lo Bosco <maurizio.lobosco@gmail.com>
2022-07-08 17:09:40 +0200

    adapt text field and introduce themes

--- yt-dl-ui.py
+++ yt-dl-ui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 import subprocess
 from DownloadFacade import DownloadFacade
 
@@ -7,17 +8,40 @@ class App(tk.Tk):
     def __init__(self, root, download_facade):
         root.title('Youtube-dl GUI')
 
+        # Create a style
+        style = ttk.Style(root)
+
+        print(style.theme_names())
+        # Set the theme with the theme_use method
+        style.theme_use('aqua')
+
         # place a label on the root window
-        tk.Label(root, text=download_facade.get_downloader()).pack()
+        ttk.Label(root, text=download_facade.get_downloader()).pack()
         url_string = tk.StringVar(root, value='https://www.youtube.com/watch?v=K096sx5whTE')
-        url_entry = tk.Entry(root, textvariable=url_string)
-        url_entry.pack()
+        url_entry = ttk.Entry(root, textvariable=url_string)
+        url_entry.pack(expand=True, fill='both', )
 
-        dw_button = tk.Button(root, text="Download", command=lambda: download_facade.download(url_string))
+        dw_button = ttk.Button(root, text="Download", command=lambda: download_facade.download(url_string))
         dw_button.pack()
 
-        dw_button = tk.Button(root, text="Paste", command=lambda: get_clipboard(url_string, root))
+        dw_button = ttk.Button(root, text="Paste", command=lambda: get_clipboard(url_string, root))
         dw_button.pack()
+        root.selected_theme = tk.StringVar()
+        theme_frame = ttk.LabelFrame(root, text='Themes')
+        for theme_name in style.theme_names():
+            # Create a bulk of radio buttons using loop
+            radio_buttons = ttk.Radiobutton(
+                root,
+                text=theme_name,
+                value=theme_name,
+                variable=root.selected_theme,
+                command=lambda: theme_changer(style, root))
+            radio_buttons.pack(expand=True, fill='both')
+
+
+def theme_changer(style, window):
+    # Change  theme
+    style.theme_use(window.selected_theme.get())
 
 
 def get_clipboard(url_string, root):
